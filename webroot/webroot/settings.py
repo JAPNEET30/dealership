75f439d19ce9559d20c633ef7810cd2165e3efa5
JAPNEET30 <japneetsinghjdp@gmail.com>
2024-06-13 18:40:02 +0530

    basic web skeleton created in webcore
    Webcore skeleton and side bar css created.

--- webroot/webroot/settings.py
+++ webroot/webroot/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webcore.apps.WebcoreConfig',
+    'usercore.apps.UsercoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -115,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = Path(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
 # Default primary key field type
--- webroot/webroot/urls.py
+++ webroot/webroot/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns +=[
+    path('', include('webcore.urls')),
+    path('login/', include('usercore.urls')),
+    path('', RedirectView.as_view(url='/', permanent=True))
 ]
